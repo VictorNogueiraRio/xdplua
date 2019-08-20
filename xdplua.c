f4dc9b5b0a7a9dae706cc34a2ba0e1c75e1e327f
Victor Nogueira <victor.nogueira@me.com>
2019-08-20 17:40:50 -0300

    Add support for interface forwarding

--- xdplua.c
+++ xdplua.c
@@ -87,8 +87,113 @@ static int xdplua_udp_reply(lua_State *L) {
 	return 1;
 }
 
+static int xdplua_fib_lookup(lua_State *L) {
+	struct xdp_rxq_info rxq;
+	struct sk_buff *skb;
+	struct xdp_buff ctx;
+	struct bpf_fib_lookup fib_params;
+	static const struct bpf_func_proto *fib_lookup_proto;
+	struct iphdr *iph;
+	int ret;
+
+	unsigned int ipdaddr = luaL_checkinteger(L, 1);
+	luaU_getregval(L, XDPLUA_SKBENTRY, &skb);
+
+	/* work around to use helper */
+	rxq.dev = skb->dev;
+	ctx.rxq = &rxq;
+	fib_lookup_proto = xdp_verifier_ops.get_func_proto(BPF_FUNC_fib_lookup,
+				NULL);
+	skb_reset_network_header(skb);
+	iph = ip_hdr(skb);
+
+	fib_params.family	= AF_INET;
+	fib_params.tos		= iph->tos;
+	fib_params.l4_protocol	= iph->protocol;
+	fib_params.sport	= 0;
+	fib_params.dport	= 0;
+	fib_params.tot_len	= ntohs(iph->tot_len);
+	fib_params.ipv4_src	= iph->saddr;
+	fib_params.ipv4_dst	= ntohl(ipdaddr);
+
+	fib_params.ifindex = skb->dev->ifindex;
+
+	ret = fib_lookup_proto->func((u64)&ctx, (u64)&fib_params,
+				(u64)sizeof(fib_params), (u64)0, (u64)0);
+
+	if (!ret) {
+		struct ethhdr *eth;
+		struct in_device* in_dev;
+		struct in_ifaddr* if_info;
+		struct net_device *fwd;
+
+		eth = (struct ethhdr *) skb_mac_header(skb);
+		memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+		memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+
+		fwd = dev_get_by_index_rcu(dev_net(skb->dev), fib_params.ifindex);
+		in_dev = (struct in_device *) fwd->ip_ptr;
+		if_info = in_dev->ifa_list;
+		for (;if_info;if_info=if_info->ifa_next) {
+			if (!(strcmp(if_info->ifa_label, fwd->name))) {
+				break;
+			}
+		}
+
+		iph->saddr = if_info->ifa_address;
+		iph->daddr = ntohs(ipdaddr);
+		lua_pushinteger(L, fib_params.ifindex);
+
+		return 1;
+	}
+
+	return 0;
+}
+
+static int xdplua_do_redirect(lua_State *L) {
+
+	struct sk_buff *skb;
+	struct iphdr *iph;
+	struct udphdr *uh;
+	struct bpf_redirect_info *ri = this_cpu_ptr(&bpf_redirect_info);
+	unsigned int ifindex = luaL_checkinteger(L, 1);
+	short udplen;
+
+	luaU_getregval(L, XDPLUA_SKBENTRY, &skb);
+
+	skb_reset_network_header(skb);
+	iph = ip_hdr(skb);
+	iph->check = 0;
+	iph->check = ip_fast_csum((unsigned char *) iph, iph->ihl);
+
+	skb_set_transport_header(skb, iph->ihl * 4);
+	uh = udp_hdr(skb);
+
+	udplen = htons(uh->len);
+	uh->check = 0;
+	uh->check = csum_tcpudp_magic(iph->saddr, iph->daddr, udplen,
+		IPPROTO_UDP, csum_partial((unsigned char *)uh, udplen, 0));
+
+	ri->ifindex = ifindex;
+	ri->flags = 0;
+	WRITE_ONCE(ri->map, NULL);
+	return 0;
+}
+
+static int xdplua_get_ifindex(lua_State *L) {
+	struct sk_buff *skb;
+
+	luaU_getregval(L, XDPLUA_SKBENTRY, &skb);
+	lua_pushinteger(L, skb->dev->ifindex);
+
+	return 1;
+}
+
 static const luaL_Reg xdplua_lib[] = {
 	{"udp_reply", xdplua_udp_reply},
+	{"fib_lookup" , xdplua_fib_lookup},
+	{"get_ifindex" , xdplua_get_ifindex},
+	{"do_redirect" , xdplua_do_redirect},
 	{NULL, NULL}
 };
 
--- xdplua.h
+++ xdplua.h
@@ -9,6 +9,14 @@
 #include <linux/skbuff.h>
 #include <net/udp.h>
 #include <net/ip.h>
+#include <linux/if_ether.h>
+#include <linux/bpf.h>
+#include <linux/filter.h>
+#include <linux/netdevice.h>
+#include <net/fib_rules.h>
+#include <net/ip_fib.h>
+#include <net/net_namespace.h>
+#include <linux/inetdevice.h>
 
 #define luaU_setregval(L, t, v) { \
 	if (v) lua_pushlightuserdata(L, v); \
